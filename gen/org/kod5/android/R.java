716b592e523251ec378a817493e968bfabef0b54
mursidyazar <mursidyazar@gmail.com>
2014-10-13 12:10:36 +0300

    Android-Spinner

--- gen/org/kod5/android/R.java
+++ gen/org/kod5/android/R.java
@@ -25,9 +25,10 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int textView1=0x7f070001;
+        public static final int spinner1=0x7f070001;
+        public static final int spinner2=0x7f070003;
+        public static final int textView1=0x7f070000;
         public static final int textView2=0x7f070002;
-        public static final int webView1=0x7f070000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
--- src/org/kod5/android/MainActivity.java
+++ src/org/kod5/android/MainActivity.java
@@ -1,54 +1,89 @@
 package org.kod5.android;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.Toast;
 
+@SuppressLint({ "NewApi", "ShowToast" })
 public class MainActivity extends Activity {
 
-	private WebView webView;
-
-	@Override
-	protected void onCreate(Bundle savedInstanceState) { // Bu metod uygulama
-															// aç›ld›€›nda
-															// çal›ﬂt›r›lan
-															// metod.
+	//Spinner içerisine koyaca€›m›z verileri tan›ml›yoruz.
+	private String[] iller={"‹STANBUL","ANKARA"};
+	private String[] ilceler0={"ADALAR","ARNAVUTKÖY","ATAﬁEH‹R","AVCILAR","BA⁄CILAR","BAHÇEL‹EVLER","BAKIRKÖY","BAﬁAKﬁEH‹R","BAYRAMPAﬁA","BEﬁ‹KTAﬁ","BEYL‹KDÜZÜ","BEYO⁄LU","BÜYÜKÇEKMECE","BEYKOZ","ÇATALCA","ÇEKMEKÖY","ESENLER","ESENYURT","EYÜP","FAT‹H","GAZ‹OSMANPAﬁA","GÜNGÖREN","KADIKÖY","KA⁄ITHANE","KARTAL","KÜÇÜKÇEKMECE","MALTEPE","PEND‹K","SANCAKTEPE","SARIYER","S‹L‹VR‹","SULTANBEYL‹","SULTANGAZ‹","ﬁ‹LE","ﬁ‹ﬁL‹","TUZLA","ÜSKÜDAR","ÜMRAN‹YE","ZEYT‹NBURNU"};
+	private String[] ilceler1={"AKYURT","ALTINDA⁄","AYAﬁ","BALA","BEYPAZARI","ÇAMLIDERE","ÇANKAYA","ÇUBUK","ELMADA⁄","ET‹MESGUT","EVREN","GÖLBAﬁI","GÜDÜL","HAYMANA","KALEC‹K","KAZAN","KEÇ‹ÖREN","KIZILCAHAMAM","MAMAK","NALLIHAN","POLATLI","PURSAKLAR","S‹NCAN","ﬁEREFL‹KOÇH‹SAR","YEN‹MAHALLE"};
+	
+	//Spinner'lar› ve Adapter'lerini tan›ml›yoruz.
+	private Spinner spinnerIller;
+	private Spinner spinnerIlceler;
+	private ArrayAdapter<String> dataAdapterForIller; 
+	private ArrayAdapter<String> dataAdapterForIlceler;
+	
+	
+	@Override // Bu metod uygulama aç›ld›€›nda çal›ﬂt›r›lan metod.
+	protected void onCreate(Bundle savedInstanceState) { 
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		// webView'i tasar›mdakiyle ba€l›yoruz.
-		webView = (WebView) findViewById(R.id.webView1);
+		//xml k›sm›nda haz›rlad›€›m›z spinnerlar› burda tan›mlad›klar›m›zla eﬂleﬂtiriyoruz.
+		spinnerIller = (Spinner) findViewById(R.id.spinner1);
+		spinnerIlceler = (Spinner) findViewById(R.id.spinner2);
+		 
+        //Spinner'lar için adapterleri haz›rl›yoruz. 
+        dataAdapterForIller = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, iller);
+        dataAdapterForIlceler = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,ilceler0);
+        
+        //Listelenecek verilerin görünümünü belirliyoruz.
+        dataAdapterForIller.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        dataAdapterForIlceler.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+ 
+        //Haz›rlad›€›m›z Adapter'leri Spinner'lara ekliyoruz.
+        spinnerIller.setAdapter(dataAdapterForIller);
+        spinnerIlceler.setAdapter(dataAdapterForIlceler);
+        
+        // Listelerden bir eleman seçildi€inde yap›lacaklar› tan›ml›yoruz.
+		spinnerIller.setOnItemSelectedListener(new OnItemSelectedListener() {
 
-		// webView'i JavaScript kodlar›n› çal›ﬂt›racak ﬂekilde set ediyoruz.
-		webView.getSettings().setJavaScriptEnabled(true);
-
-		// ‹ﬂlem yap›ld›€›n›n anlaﬂ›lmas› için ProgressDialog aç›yoruz.
-		final ProgressDialog progressDialog = ProgressDialog.show(this, "KOD5",
-				"Sayfa Yükleniyor...", true);
-		
-		webView.setWebViewClient(new WebViewClient() {
-
-			// Sayfa Yüklenirken bir hata oluﬂursa kullan›c›y› uyar›yoruz.
-			public void onReceivedError(WebView view, int errorCode,
-					String description, String failingUrl) {
-				Toast.makeText(getApplicationContext(), "Sayfa Yüklenemedi!",
-						Toast.LENGTH_SHORT).show();
+			@Override
+			public void onItemSelected(AdapterView<?> parent, View view,
+					int position, long id) {
+				//Hangi il seçilmiﬂse onun ilçeleri adapter'e ekleniyor.
+				if(parent.getSelectedItem().toString().equals(iller[0])) 
+					dataAdapterForIlceler = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_spinner_item,ilceler0);
+				else if(parent.getSelectedItem().toString().equals(iller[1]))
+					dataAdapterForIlceler = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_spinner_item,ilceler1);
+				
+				dataAdapterForIlceler.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+				spinnerIlceler.setAdapter(dataAdapterForIlceler);
 			}
 
-			// Sayfan›n yüklenme iﬂlemi bitti€inde progressDialog'u kapat›yoruz.
 			@Override
-			public void onPageFinished(WebView view, String url) {
-				super.onPageFinished(view, url);
-				if (progressDialog.isShowing())
-					progressDialog.dismiss();
+			public void onNothingSelected(AdapterView<?> parent) {
 			}
 		});
+		
+		spinnerIlceler.setOnItemSelectedListener(new OnItemSelectedListener() {
 
-		//Web sayfam›z›n url'ini webView'e yüklüyoruz.
-		webView.loadUrl("http://www.kod5.org");
+					@Override
+					public void onItemSelected(AdapterView<?> parent, View view,
+							int position, long id) {
+						//Seçilen il ve ilçeyi ekranda gösteriyoruz.
+						Toast.makeText(getBaseContext(), ""+spinnerIller.getSelectedItem().toString()+"\n"+parent.getSelectedItem().toString(), Toast.LENGTH_SHORT).show();
+					}
 
+					@Override
+					public void onNothingSelected(AdapterView<?> parent) {
+					}
+				});
 	}
+
+	
 }
